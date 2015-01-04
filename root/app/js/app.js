ede7db284022e0ff5e4ef76f2da9eab83d8325b0
paulsouche <souche.p@sfeir.com>
2015-01-04 18:34:22 +0100

    introducing controller as syntax

--- root/app/js/app.js
+++ root/app/js/app.js
@@ -1,25 +1,25 @@
 angular.module('{%= name %}', {%= importedModules %});
 
 {%if (importedModules.indexOf('ui.router') !== -1 && importedModules.indexOf('pascalprecht.translate') === -1) {%}
-angular.module('{%= name %}').config(function($stateProvider, $urlRouterProvider) {
+angular.module('{%= name %}').config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
 
   'use strict';
 
-});
+}]);
 {%}%}
 
 {%if (importedModules.indexOf('pascalprecht.translate') !== -1 && importedModules.indexOf('ui.router') === -1) {%}
-angular.module('{%= name %}').config(function($translateProvider) {
+angular.module('{%= name %}').config(['$translateProvider',function($translateProvider) {
 
   'use strict';
 
-});
+}]);
 {%}%}
 
 {%if (importedModules.indexOf('ui.router') !== -1 && importedModules.indexOf('pascalprecht.translate') !== -1) {%}
-angular.module('{%= name %}').config(function($stateProvider, $urlRouterProvider, $translateProvider) {
+angular.module('{%= name %}').config(['$stateProvider', '$urlRouterProvider', '$translateProvider',function($stateProvider, $urlRouterProvider, $translateProvider) {
 
   'use strict';
 
-});
+}]);
 {%}%}
